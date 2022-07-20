9086dc48041c904cc63877b8b66bd9589ad11a21
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-07-20 20:22:14 +0200

    font change

---
+++ components/fonts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Global } from '@emotion/react'
+
+const Fonts = () => {
+  return (
+    <Global
+      styles={`@import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300&family=Source+Code+Pro&display=swap')`}
+    />
+  )
+}
+
+export default Fonts
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/layout/Main'
 import theme from '@chakra-ui/react'
+import Fonts from '../components/fonts'
 const Website = ({ Component, pageProps, router }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Fonts />
       <Layout router={router}>
         <Component {...pageProps} key={router.route} />
       </Layout>
