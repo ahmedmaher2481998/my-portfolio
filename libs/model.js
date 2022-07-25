c8e2f44a3c766f44d4a82f256ad38fc0013eae38
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-07-25 18:47:56 +0200

    model loader

--- libs/model.js
+++ libs/model.js
@@ -4,8 +4,30 @@ export const loadGLTFMode = (
   glbPath,
   options = { receivedShadow: True, castShadow: true }
 ) => {
-  const { castShadow, receivedShadow } = options
-  return new Promise((resolved, rejected) => {
+  const { castShadow, receiveShadow } = options
+  return new Promise((resolve, reject) => {
     const loader = new GLTFLoader()
+    loader.load(glbPath, gtlf => {
+      const obj = gtlf.scene
+      obj.name = '3D-Model'
+      obj.position.x = 0
+      obj.position.y = 0
+      obj.receiveShadow = receiveShadow
+      obj.castShadow = castShadow
+      scene.add(obj)
+      obj.traverse(
+        child => {
+          if (child.isMesh) {
+            child.castShadow = castShadow
+            child.receiveShadow = receiveShadow
+          }
+          resolve(obj)
+        },
+        undefined,
+        err => {
+          reject(err)
+        }
+      )
+    })
   })
 }
