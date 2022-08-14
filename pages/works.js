ea1df5f16eaf8e4f061e57056de7195a43c662db
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-14 09:11:19 +0200

    added : assests

--- pages/works.js
+++ pages/works.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import SectionAnimated from '../components/section'
-import Body from '../components/layout/body'
-import { WorkGridItem } from '../components/grid'
+import SectionAnimated from '../../components/section'
+import Body from '../../components/layout/body'
+import { WorkGridItem } from '../../components/grid'
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
-import test from '../public/images/works/00.png'
+import test from '../../public/images/works/inkdrop_eyecatch.png'
 const Works = () => {
   return (
     <>
@@ -14,12 +14,12 @@ const Works = () => {
         <SimpleGrid column={[1, 2, 2]} gap={8}>
           <p>this is works </p>
           {/* Works goes in here */}
-          {/* <SectionAnimated delay={0.2}>
-          <WorkGridItem id="blabla" title="blabla blabla " src={test}>
-            This is a test work
+          <SectionAnimated delay={0.2}>
+            <WorkGridItem title="this is an image from the project i worked on  " src={test}>
+              This is a test work
             </WorkGridItem>
             <p>test</p>
-          </SectionAnimated> */}
+          </SectionAnimated>
           <Divider my={6} />
         </SimpleGrid>
       </Body>
