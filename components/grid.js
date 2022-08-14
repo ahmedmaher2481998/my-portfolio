dd2989e4dc92d6a7e0fb546671ead030071d6cc7
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-08-14 10:23:44 +0200

    fixed : error in the works page

--- components/grid.js
+++ components/grid.js
@@ -26,34 +26,32 @@ export const GridItem = ({ children, href, title, src }) => {
 }
 export const WorkGridItem = ({ children, id, title, src }) => {
   return (
-    <Box w="100%" textAlign={'center'}>
-      <NextLink href={`/works/${id}`} passHref scroll={false}>
-        <LinkBox cursor={'pointer'}>
-          <Image
-            src={src}
-            alt={title}
-            className="grid-item-thumbnail"
-            placeholder="blur"
-          />
-          <LinkOverlay ref={`/works/${id}`}>
-            <Text fontSize={20} mt={2}>
-              {title}
-            </Text>
-            <Text mt={2} fontSize={14}>
-              {children}
-            </Text>
-          </LinkOverlay>
-        </LinkBox>
-      </NextLink>
-    </Box>
+    <>
+      <Box w="100%" textAlign="center">
+        <NextLink href={`/works/${id}`} passHref scroll={false}>
+          <LinkBox cursor={'pointer'}>
+            <Image src={src} alt={title} className="grid-item-thumbnail" placeholder="blur" />
+            <LinkOverlay href={`/works/${id}`}>
+              <Text fontSize={20} mt={2}>
+                {title}
+              </Text>
+            </LinkOverlay>
+            <Text fontSize={14}> {children} </Text>
+          </LinkBox>
+        </NextLink>
+      </Box>
+    </>
   )
 }
+
 export const GridItemStyle = () => {
-  ;<Global
-    styles={`
+  return (
+    <Global
+      styles={`
 .grid-item-thumbnail{ 
-      border-radius: 20px;
+    border-radius: 12px;
 }
 `}
-  />
+    />
+  )
 }
--- pages/works.js
+++ pages/works.js
@@ -1,27 +1,37 @@
-import React from 'react'
-import SectionAnimated from '../../components/section'
-import Body from '../../components/layout/body'
-import { WorkGridItem } from '../../components/grid'
+import SectionAnimated from '../components/section'
+import Body from '../components/layout/body'
+import { WorkGridItem } from '../components/grid'
 import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
-import test from '../../public/images/works/inkdrop_eyecatch.png'
+import homepage from '../public/images/e-commerce/home-page.png'
+import battleShip from '../public/images/battleship-images/welcome-xl.png'
+import { createRef } from 'react'
+import { forwardRef } from 'react'
+
 const Works = () => {
   return (
     <>
       <Body title="Works">
-        <Heading as={'h3'} fontSize={20} mb={4}>
-          Works
-        </Heading>
-        <SimpleGrid column={[1, 2, 2]} gap={8}>
-          <p>this is works </p>
-          {/* Works goes in here */}
-          <SectionAnimated delay={0.2}>
-            <WorkGridItem title="this is an image from the project i worked on  " src={test}>
-              This is a test work
-            </WorkGridItem>
-            <p>test</p>
-          </SectionAnimated>
-          <Divider my={6} />
-        </SimpleGrid>
+        <Container>
+          <Heading as={'h3'} fontSize={20} mb={4}>
+            Works
+          </Heading>
+
+          <SimpleGrid column={[1, 1, 2]} gap={6}>
+            {/* <p>this is works </p> */}
+            {/* Works goes in here */}
+            <SectionAnimated delay={0.2}>
+              <WorkGridItem
+                title="this is an image from the project i worked on  "
+                id={'123'}
+                src={homepage}
+              >
+                A Furniture E-commerce with connected with stripe and has advanced filter{' '}
+              </WorkGridItem>
+              <p>test</p>
+            </SectionAnimated>
+            <Divider my={6} />
+          </SimpleGrid>
+        </Container>
       </Body>
     </>
   )
